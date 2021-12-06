b9f8cf837eb9940b0060e436a3473a5b5f40bf88
alhe1002 <alhe1002@stud.hs-kl.de>
2021-12-06 16:36:01 +0100

    The main class changed

---
+++ src/main/java/de/hs_kl/staab/MainApplication.java
@@ -0,0 +1,45 @@
+package de.hs_kl.staab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.hs_kl.staab.planner.Address;
+import de.hs_kl.staab.planner.Workshop;
+
+/**
+ * <p>
+ * This class contains the main function and is called upon start. It starts
+ * Spring Boot, which starts an application server and runs your application in
+ * this application server so that it can be reached in the Network.
+ * </p>
+ * 
+ * <p>
+ * If you now enter the following URL in the browser, you can access the
+ * Swagger-Interface of the application: <a href=
+ * "http://localhost:8080/swagger-ui/">http://localhost:8080/swagger-ui/</a>.
+ * </p>
+ *
+ * @author Staab
+ */
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Now the application is started!");
+		// Wenn Sie OHNE REST arbeiten wollen, können Sie die folgende Zeile
+		// und die Klasse PlannerController löschen!
+		SpringApplication.run(MainApplication.class, args);
+
+		// Wenn Sie MIT REST arbeiten wollen, können Sie die folgende Zeile
+		// und die entsprechende Methode löschen!
+		runApplication();
+	}
+
+	private static void runApplication() {
+		Address addressOfWorkshop = new Address("Industriestraße", 20, 66000, "Zweibrücken");
+		Workshop workshop = new Workshop("Auto Werkstatt GmbH", addressOfWorkshop);
+
+		System.out.println(workshop);
+	}
+
+}
