856c1ad2905eca0f20735a90746e9298a3e81e57
alhe1002 <alhe1002@stud.hs-kl.de>
2021-12-06 16:21:14 +0100

    classes reuploaded

--- src/main/java/de/hs_kl/staab/planner/Address.java
+++ src/main/java/de/hs_kl/staab/planner/Address.java
@@ -2,4 +2,32 @@ package de.hs_kl.staab.planner;
 
 public class Address {
 
+	private final String streetName;
+	private final int streetNumber;
+	private final int zipCode;
+	private final String city;
+
+	public Address(String streetName, int streetNumber, int zipCode, String city) {
+		super();
+		this.streetName = streetName;
+		this.streetNumber = streetNumber;
+		this.zipCode = zipCode;
+		this.city = city;
+	}
+
+	public String getStreetName() {
+		return streetName;
+	}
+
+	public int getStreetNumber() {
+		return streetNumber;
+	}
+
+	public int getZipCode() {
+		return zipCode;
+	}
+
+	public String getCity() {
+		return city;
+	}
 }
--- src/main/java/de/hs_kl/staab/planner/Workshop.java
+++ src/main/java/de/hs_kl/staab/planner/Workshop.java
@@ -2,4 +2,17 @@ package de.hs_kl.staab.planner;
 
 public class Workshop {
 
+	private final static String TEXT_PREFIX = "WS-";
+	private static int ID_COUNTER = 1;
+
+	private final String idOfWorkshop;
+	private final String nameOfWorkshop;
+	private final Address addressOfWorkshop;
+
+	public Workshop(String nameOfWorkshop, Address addressOfWorkshop) {
+		this.idOfWorkshop = TEXT_PREFIX + ID_COUNTER;
+		ID_COUNTER++;
+		this.nameOfWorkshop = nameOfWorkshop;
+		this.addressOfWorkshop = addressOfWorkshop;
+	}
 }
