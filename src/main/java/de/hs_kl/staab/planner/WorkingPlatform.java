646555cedb4591c8fa43225c82637451d6f7c61e
alhe1002 <alhe1002@stud.hs-kl.de>
2021-12-06 16:48:12 +0100

    The working platform class extended with properties

--- src/main/java/de/hs_kl/staab/planner/WorkingPlatform.java
+++ src/main/java/de/hs_kl/staab/planner/WorkingPlatform.java
@@ -2,4 +2,12 @@ package de.hs_kl.staab.planner;
 
 public class WorkingPlatform {
 
+	private final static String WORKING_PLATFORM_PREFIX = "WKP-";
+	private static int WORKING_PLATFORM_ID_COUNTER = 1;
+
+	private final String idOfWorkingPlatform;
+	private final String nameOfWorkingPlatform;
+	private boolean isWorkingPlatformAvailable;
+	private Workshop workshop;
+
 }
