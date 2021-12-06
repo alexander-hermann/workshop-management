1557e13b4d103da554c50b1f6d32d6c95688561a
alhe1002 <alhe1002@stud.hs-kl.de>
2021-12-06 16:44:01 +0100

    Small changes of the workshop class

--- src/main/java/de/hs_kl/staab/planner/Workshop.java
+++ src/main/java/de/hs_kl/staab/planner/Workshop.java
@@ -2,16 +2,16 @@ package de.hs_kl.staab.planner;
 
 public class Workshop {
 
-	private final static String TEXT_PREFIX = "WS-";
-	private static int ID_COUNTER = 1;
+	private final static String WORKSHOP_PREFIX = "WS-";
+	private static int WORKSHOP_ID_COUNTER = 1;
 
 	private final String idOfWorkshop;
 	private final String nameOfWorkshop;
 	private final Address addressOfWorkshop;
 
 	public Workshop(String nameOfWorkshop, Address addressOfWorkshop) {
-		this.idOfWorkshop = TEXT_PREFIX + ID_COUNTER;
-		ID_COUNTER++;
+		this.idOfWorkshop = WORKSHOP_PREFIX + WORKSHOP_ID_COUNTER;
+		WORKSHOP_ID_COUNTER++;
 		this.nameOfWorkshop = nameOfWorkshop;
 		this.addressOfWorkshop = addressOfWorkshop;
 	}
