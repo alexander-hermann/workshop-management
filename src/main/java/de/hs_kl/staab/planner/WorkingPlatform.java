1f2020ebd937f78117d73480bf15e91a29a41d9a
alhe1002 <alhe1002@stud.hs-kl.de>
2021-12-08 20:18:46 +0100

    workingplatform updated

--- src/main/java/de/hs_kl/staab/planner/WorkingPlatform.java
+++ src/main/java/de/hs_kl/staab/planner/WorkingPlatform.java
@@ -1,27 +1,29 @@
 package de.hs_kl.staab.planner;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class WorkingPlatform {
 
 	private final static String WORKING_PLATFORM_PREFIX = "WKP-";
 	private static int WORKING_PLATFORM_ID_COUNTER = 1;
 
-	private final String idOfWorkingPlatform;
+	private String idOfWorkingPlatform;
 	private final String nameOfWorkingPlatform;
 
-	private ArrayList<WorkingPlatform> listOfWorkingPlatforms;
+	private static List<WorkingPlatform> listOfWorkingPlatforms = new ArrayList<>();
 
 	public WorkingPlatform(String nameOfWorkingPlatform) {
 		this.idOfWorkingPlatform = WORKING_PLATFORM_PREFIX + WORKING_PLATFORM_ID_COUNTER;
+		WORKING_PLATFORM_ID_COUNTER++;
 		this.nameOfWorkingPlatform = nameOfWorkingPlatform;
 	}
 
-	public String getIdOfWorkingPlatform() {
+	public String getId() {
 		return idOfWorkingPlatform;
 	}
 
-	public String getNameOfWorkingPlatform() {
+	public String getName() {
 		return nameOfWorkingPlatform;
 	}
 
@@ -36,12 +38,54 @@ public class WorkingPlatform {
 		return builder.toString();
 	}
 
-	public ArrayList<WorkingPlatform> getWorkingPlatforms() {
-		return listOfWorkingPlatforms;
+	public static WorkingPlatform getWorkingPlatformById(int workingPlatformId) {
+		String addPrefixtoId = WORKING_PLATFORM_PREFIX + workingPlatformId;
+		if (!listOfWorkingPlatforms.isEmpty()) {
+			for (WorkingPlatform workingPlatform : listOfWorkingPlatforms) {
+				if (workingPlatform.getId().equals(addPrefixtoId)) {
+					return workingPlatform;
+				}
+			}
+		} else {
+			throw new IllegalArgumentException("The list of working platform is empty.");
+		}
+		throw new IllegalArgumentException("The workingplatform with the id " + workingPlatformId + " is not found.");
 	}
 
-	public void createNewWorkingPlatform(WorkingPlatform workingPlatform) {
-		listOfWorkingPlatforms = new ArrayList<>();
-		listOfWorkingPlatforms.add(workingPlatform);
+	public static void getWorkingPlatforms() {
+		if (!listOfWorkingPlatforms.isEmpty()) {
+			for (WorkingPlatform workingPlatform : listOfWorkingPlatforms) {
+				System.out.println(workingPlatform);
+			}
+		} else {
+			throw new IllegalArgumentException("The list of workingplatforms is empty.");
+		}
+	}
+
+	public static void createNewWorkingPlatform(WorkingPlatform newWorkingPlatform) {
+		listOfWorkingPlatforms.add(newWorkingPlatform);
+		System.out.println("Erfolgreich");
+	}
+
+	public void updateWorkingPlatform(WorkingPlatform updateWorkingPlatform) {
+		if (!listOfWorkingPlatforms.isEmpty()) {
+			for (WorkingPlatform workingPlatform : listOfWorkingPlatforms) {
+				if (workingPlatform.equals(this)) {
+					updateWorkingPlatform.idOfWorkingPlatform = this.idOfWorkingPlatform;
+
+					System.out.println("Old object: " + workingPlatform);
+					listOfWorkingPlatforms.set(listOfWorkingPlatforms.indexOf(this), updateWorkingPlatform);
+					System.out.println("New object: " + updateWorkingPlatform);
+				}
+			}
+		} else {
+			throw new IllegalArgumentException("The workingplatform can not be updated, because the list is empty.");
+		}
+	}
+
+	public void removeWorkingPlatform(WorkingPlatform removeWorkingPlatform) {
+		if (!listOfWorkingPlatforms.isEmpty()) {
+
+		}
 	}
 }
