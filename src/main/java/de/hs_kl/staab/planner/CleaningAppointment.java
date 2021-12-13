fee2e768d94554a5a92e9a9d2f0bc29cd7a4a023
Ammar <Ammar@192.168.2.199>
2021-12-13 13:40:27 +0100

    neu erstellung von classen und überarbeitung von Appointment

---
+++ src/main/java/de/hs_kl/staab/planner/CleaningAppointment.java
@@ -0,0 +1,12 @@
+package de.hs_kl.staab.planner;
+
+import java.time.LocalDateTime;
+
+public class CleaningAppointment extends Appointment {
+
+	public CleaningAppointment(LocalDateTime DateOfAppointment, int calenderWeekOfAppointment) {
+		super(DateOfAppointment, calenderWeekOfAppointment);
+		// TODO Auto-generated constructor stub
+	}
+
+}
