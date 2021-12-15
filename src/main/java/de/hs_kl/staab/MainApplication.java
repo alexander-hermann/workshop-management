f7376b11f85bb9550bea8cce5469f197cbf3c285
alhe1002 <alhe1002@stud.hs-kl.de>
2021-12-15 10:22:07 +0100

    The main application was cleaned up

--- src/main/java/de/hs_kl/staab/MainApplication.java
+++ src/main/java/de/hs_kl/staab/MainApplication.java
@@ -1,8 +1,5 @@
 package de.hs_kl.staab;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import de.hs_kl.staab.planner.CLEANINGPROGRAMM;
@@ -51,7 +48,7 @@ public class MainApplication {
 	private static void runApplication() {
 		PlannerService plannerService = PlannerService.getInstance();
 		PlanningCalendar planningCalendar = new PlanningCalendar();
-		CustomerService customerservice = new CustomerService();
+		CustomerService customerService = new CustomerService();
 
 		WorkingPlatform wkp1 = new WorkingPlatform("Arbeitsbühne1");
 		WorkingPlatform wkp2 = new WorkingPlatform("Arbeitsbühne2");
@@ -62,21 +59,20 @@ public class MainApplication {
 		Service glassRepair = new Service("Glasreparatur", 2.0);
 		Service inspectionCar = new Service("KFZ-Inspektion", 1.5);
 
-		InvoiceAddress Ammar = new InvoiceAddress("Hauptstraße", 20, 66840, "Zweibrücken");
+		InvoiceAddress addressOfSaid = new InvoiceAddress("Hauptstraße", 20, 66840, "Zweibrücken");
+		InvoiceAddress addressOfTreitz = new InvoiceAddress("Hauptstraße", 15, 66820, "Bierbach");
+		InvoiceAddress addressOfHermann = new InvoiceAddress("Hauptstraße", 30, 66880, "Kaiserslautern");
 
 		Vehicle car1 = new Car("HOM-SB-211", "BMW", "M1", 2016, 2018, 8, 1);
 		Vehicle car2 = new Car("SB-HO-333", "Mercedes", "CLA", 2010, 2013, 8, 1);
-		customerservice.createNewVehicle(car1);
-		customerservice.createNewVehicle(car2);
-		List<Vehicle> listOfVehiclesForAmmar = new ArrayList<>();
-		listOfVehiclesForAmmar.add(car1);
-		customerservice.getListOfVehicles();
 
-		Customer customer1 = new Customer("Ammar Said", Ammar, "+49176 258484750", "ammar@web.de",
-				listOfVehiclesForAmmar);
-		Customer customer2 = new Customer("Alexander Hermann", null, "0176-5656556", "alex@web.de", null);
-		Customer customer3 = new Customer("Hendrik Treitz", null, "+49176-4548484", "hendrik@web.de", null);
-		customerservice.createNewCustomer(customer1);
+		customerService.createNewVehicle(car1);
+		customerService.createNewVehicle(car2);
+
+		Customer customer1 = new Customer("Ammar Said", addressOfSaid, "+49176 258484750", "ammar@web.de", null);
+		Customer customer2 = new Customer("Alexander Hermann", addressOfHermann, "0176-5656556", "alex@web.de", null);
+		Customer customer3 = new Customer("Hendrik Treitz", addressOfTreitz, "+49176-4548484", "hendrik@web.de", null);
+
 		customer1.getListOfVehiclesForCustomer();
 		// customerservice.updateCustomer("HoM", customer1);
 
