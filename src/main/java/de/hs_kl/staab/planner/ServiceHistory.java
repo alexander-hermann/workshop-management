9e59cef82a970b531d3fbcc71d3c9c4d8e84531d
alhe1002 <alhe1002@stud.hs-kl.de>
2021-12-18 08:39:14 +0100

    The class service history extended with attributes

--- src/main/java/de/hs_kl/staab/planner/ServiceHistory.java
+++ src/main/java/de/hs_kl/staab/planner/ServiceHistory.java
@@ -1,5 +1,11 @@
 package de.hs_kl.staab.planner;
 
+import java.time.LocalDateTime;
+
 public class ServiceHistory {
+	private final Vehicle vehicle;
+	private final Service service;
+	private final User employee;
+	private final LocalDateTime date;
 
 }
