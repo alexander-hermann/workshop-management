ff1e6a0d430ffde681fdfc88be46e8e86fae37da
alhe1002 <alhe1002@stud.hs-kl.de>
2021-12-21 16:25:59 +0100

    The class (terminable) was updated

--- src/main/java/de/hs_kl/staab/planner/Terminable.java
+++ src/main/java/de/hs_kl/staab/planner/Terminable.java
@@ -2,5 +2,9 @@ package de.hs_kl.staab.planner;
 
 public interface Terminable {
 
+	public int getDuration();
+
+	public boolean isAppointmentAvailableInPeriode(Appointment appointment);
+
 	public boolean isAppointmentInWorkingTime(Appointment appointment);
 }
