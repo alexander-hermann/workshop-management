cc16b233d8c702cf5854be6d256a1666b3516541
alhe1002 <alhe1002@stud.hs-kl.de>
2021-12-21 14:30:30 +0100

    Added the class (appoinment) for a test

---
+++ src/test/java/de/hs_kl/staab/planner/Appointment.java
@@ -0,0 +1,14 @@
+package de.hs_kl.staab.planner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Appointment {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
