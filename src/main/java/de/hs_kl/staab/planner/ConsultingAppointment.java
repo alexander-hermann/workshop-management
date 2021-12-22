1acc269b6936844f66f6a35dbcd141f6fbe57960
Ammar <Ammar@192.168.2.199>
2021-12-22 15:36:01 +0100

    MMP020

--- src/main/java/de/hs_kl/staab/planner/ConsultingAppointment.java
+++ src/main/java/de/hs_kl/staab/planner/ConsultingAppointment.java
@@ -3,14 +3,20 @@ package de.hs_kl.staab.planner;
 public class ConsultingAppointment extends Appointment {
 	private final Customer customer;
 	private final double durationInH;
+	private final CustomerConsultant customerConsultant;
 
 	public ConsultingAppointment(int year, int month, int day, int hour, int minute, Customer customer,
-			double durationInH) {
+			double durationInH, CustomerConsultant customerConsultant) {
 		super(year, month, day, hour, minute);
 		this.customer = customer;
 		this.durationInH = durationInH;
+		this.customerConsultant = customerConsultant;
 
 		this.endOfAppointment = this.startOfAppointment.plusHours((long) this.durationInH);
+	}
+
+	public CustomerConsultant getCustomerConsultant() {
+		return customerConsultant;
 	}
 
 	public Customer getCustomer() {
@@ -23,7 +29,8 @@ public class ConsultingAppointment extends Appointment {
 
 	@Override
 	public String toString() {
-		return "ConsultantAppointment [" + customer + ", DurationInH=" + durationInH + "]" + super.toString();
+		return "ConsultingAppointment [customer=" + customer + ", durationInH=" + durationInH + ", customerConsultant="
+				+ customerConsultant + "]";
 	}
 
 }
