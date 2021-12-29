69fbab441cd5f7e348a0fac7f9688e846454289c
Alexander Hermann <alhe1002@stud.hs-kl.de>
2021-12-29 21:56:44 +0100

    The variable names from our customers have been changed

--- src/main/java/de/hs_kl/staab/MainApplication.java
+++ src/main/java/de/hs_kl/staab/MainApplication.java
@@ -66,18 +66,20 @@ public class MainApplication {
 		Service glassRepair = new Service("Glasreparatur", 2.0);
 		Service inspectionCar = new Service("KFZ-Inspektion", 1.5);
 
-		InvoiceAddress addressOfSaid = new InvoiceAddress("Hauptstraße", 20, 66840, "Zweibrücken");
-		InvoiceAddress addressOfTreitz = new InvoiceAddress("Hauptstraße", 15, 66820, "Bierbach");
-		InvoiceAddress addressOfHermann = new InvoiceAddress("Hauptstraße", 30, 66880, "Kaiserslautern");
+		InvoiceAddress invoiceAddressOfCustomer1 = new InvoiceAddress("Hauptstraße", 20, 66840, "Zweibrücken");
+		InvoiceAddress invoiceAddressOfCustomer2 = new InvoiceAddress("Hauptstraße", 30, 66880, "Kaiserslautern");
+		InvoiceAddress invoiceAddressOfCustomer3 = new InvoiceAddress("Hauptstraße", 15, 66820, "Bierbach");
 
 		Vehicle car1 = new Car("HOM-SB 211", "BMW", "M1", 2016, 2018, 8, 1);
 		Vehicle car2 = new Car("SB-HO 333", "Mercedes", "CLA", 2010, 2013, 8, 1);
 		Vehicle car3 = new Car("KUS-AM 24", "Audi", "RS6", 2010, 2013, 8, 1);
 		Vehicle car4 = new Car("KL-AX 500", "Porsche", "Taycan Turbo S", 2019, 2020, 8, 2);
 
-		Customer customer1 = new Customer("Ammar Said", addressOfSaid, "+49176 258484750", "ammar@web.de");
-		Customer customer2 = new Customer("Alexander Hermann", addressOfHermann, "0176-5656556", "alex@web.de");
-		Customer customer3 = new Customer("Hendrik Treitz", addressOfTreitz, "+49176-4548484", "hendrik@web.de");
+		Customer customer1 = new Customer("Ammar Said", invoiceAddressOfCustomer1, "+49176 258484750", "ammar@web.de");
+		Customer customer2 = new Customer("Alexander Hermann", invoiceAddressOfCustomer2, "0176-5656556",
+				"alex@web.de");
+		Customer customer3 = new Customer("Hendrik Treitz", invoiceAddressOfCustomer3, "+49176-4548484",
+				"hendrik@web.de");
 
 		ConsultingAppointment consultingAppointment1 = new ConsultingAppointment(2016, 1, 5, 13, 15, customer1, 1,
 				customerConsultant1);
