f6943aaae8ab2d8fe7659e0ec45f2144c48e2f84
Alexander Hermann <alhe1002@stud.hs-kl.de>
2022-02-21 11:26:04 +0100

    The invoiceAddressTest was updated

--- src/test/java/de/hs_kl/staab/planner/InvoiceAddressTest.java
+++ src/test/java/de/hs_kl/staab/planner/InvoiceAddressTest.java
@@ -1,5 +1,19 @@
 package de.hs_kl.staab.planner;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class InvoiceAddressTest {
 
+	@Test
+	public void allInvoiceAdressTest() {
+
+		InvoiceAddress invoiceAddressOfCustomer1 = new InvoiceAddress("Hauptstraße", 20, 66840, "Zweibrücken");
+
+		assertEquals(invoiceAddressOfCustomer1.getStreetName(), invoiceAddressOfCustomer1.getStreetName());
+		assertEquals(invoiceAddressOfCustomer1.getStreetNumber(), invoiceAddressOfCustomer1.getStreetNumber());
+		assertEquals(invoiceAddressOfCustomer1.getCity(), invoiceAddressOfCustomer1.getCity());
+		assertEquals(invoiceAddressOfCustomer1.getZipCode(), invoiceAddressOfCustomer1.getZipCode());
+	}
 }
