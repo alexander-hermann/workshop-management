6cce93690c9a021b1f8c0deb4c1b7128433fa0de
Alexander Hermann <alhe1002@stud.hs-kl.de>
2022-02-21 12:16:30 +0100

    The carTest was updated

--- src/test/java/de/hs_kl/staab/planner/CarTest.java
+++ src/test/java/de/hs_kl/staab/planner/CarTest.java
@@ -1,5 +1,22 @@
 package de.hs_kl.staab.planner;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class CarTest {
 
+	@Test
+	public void allCarTest() {
+
+		InvoiceAddress invoiceAddressOfCustomer1 = new InvoiceAddress("Hauptstraße", 20, 66840, "Zweibrücken");
+		Customer customer1 = new Customer("Ammar Said", invoiceAddressOfCustomer1, "+49176 258484750", "ammar@web.de");
+		Vehicle car1 = new Car("HOM-SB 211", "BMW", "M1", 2016, 2018, 8, 1, customer1);
+
+		assertEquals(car1.getId(), car1.getId());
+		assertEquals(car1.getBrand(), car1.getBrand());
+		assertEquals(car1.getDateOfAdmission(), car1.getDateOfAdmission());
+		assertEquals(car1.getYearOfConstruction(), car1.getYearOfConstruction());
+		assertEquals(car1.getLicensePlate(), car1.getLicensePlate());
+	}
 }
