92b117ee78ece3f5c706b1ff94c6bf0e9eeb2b5e
Alexander Hermann <alhe1002@stud.hs-kl.de>
2022-02-21 12:11:42 +0100

    The dispatcherTest was updated

--- src/test/java/de/hs_kl/staab/planner/DispatcherTest.java
+++ src/test/java/de/hs_kl/staab/planner/DispatcherTest.java
@@ -1,5 +1,7 @@
 package de.hs_kl.staab.planner;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class DispatcherTest {
@@ -7,5 +9,11 @@ public class DispatcherTest {
 	@Test
 	public void allDispatcherTest() {
 
+		Dispatcher dispatcher1 = new Dispatcher("Disponent-1", "1232567890", "Herbert@ATU.de");
+
+		assertEquals(dispatcher1.getId(), dispatcher1.getId());
+		assertEquals(dispatcher1.getFullName(), dispatcher1.getFullName());
+		assertEquals(dispatcher1.getEmailAddress(), dispatcher1.getEmailAddress());
+		assertEquals(dispatcher1.getPhoneNumber(), dispatcher1.getPhoneNumber());
 	}
 }
