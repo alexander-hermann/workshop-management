f5114eb1a6e033b3e24e886e73259a2f330b4e2e
Alexander Hermann <alhe1002@stud.hs-kl.de>
2022-02-21 12:01:59 +0100

    The consultingAppointmentTest was updated

--- src/test/java/de/hs_kl/staab/planner/ConsultingAppointmentTest.java
+++ src/test/java/de/hs_kl/staab/planner/ConsultingAppointmentTest.java
@@ -1,5 +1,24 @@
 package de.hs_kl.staab.planner;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class ConsultingAppointmentTest {
 
+	@Test
+	public void allConsultingAppointmentTest() {
+
+		InvoiceAddress invoiceAddressOfCustomer1 = new InvoiceAddress("Hauptstraße", 20, 66840, "Zweibrücken");
+		Customer customer1 = new Customer("Ammar Said", invoiceAddressOfCustomer1, "+49176 258484750", "ammar@web.de");
+		CustomerConsultant customerConsultant1 = new CustomerConsultant("Kundenberater-2", "1234547890-2",
+				"Ralph@ATU.de");
+		ConsultingAppointment consultingAppointment1 = new ConsultingAppointment(2021, 12, 20, 8, 00, customer1, 1,
+				customerConsultant1);
+
+		assertEquals(consultingAppointment1.getDayWithStartTime(), consultingAppointment1.getDayWithStartTime());
+		assertEquals(consultingAppointment1.getDayWithEndTime(), consultingAppointment1.getDayWithEndTime());
+		assertEquals(consultingAppointment1.getCustomerConsultant(), consultingAppointment1.getCustomerConsultant());
+		assertEquals(consultingAppointment1.getCustomer(), consultingAppointment1.getCustomer());
+	}
 }
